d90ed22a2516de7dfa0f2ba35283c8f4973337fb
Arvind-Sabarinathan <arvind.personal2004@gmail.com>
2025-11-09 04:01:55 +0530

    Feat: Daily news summary

--- lib/nodemailer/index.ts
+++ lib/nodemailer/index.ts
@@ -1,5 +1,8 @@
 import nodemailer from "nodemailer";
-import { WELCOME_EMAIL_TEMPLATE } from "./template";
+import {
+  NEWS_SUMMARY_EMAIL_TEMPLATE,
+  WELCOME_EMAIL_TEMPLATE,
+} from "./template";
 
 export const transporter = nodemailer.createTransport({
   service: "gmail",
@@ -25,6 +28,31 @@ export const sendWelcomeEmail = async ({
     subject: "Welcome to signalist - your stock market toolkit is ready!",
     text: "Thanks for joining Signalist!",
     html: htmltemplate,
+  };
+
+  await transporter.sendMail(mailOptions);
+};
+
+export const sendNewsSummaryEmail = async ({
+  email,
+  date,
+  newsContent,
+}: {
+  email: string;
+  date: string;
+  newsContent: string;
+}): Promise<void> => {
+  const htmlTemplate = NEWS_SUMMARY_EMAIL_TEMPLATE.replace(
+    "{{date}}",
+    date,
+  ).replace("{{newsContent}}", newsContent);
+
+  const mailOptions = {
+    from: `"Signalist News" <signalist@gmail.com>`,
+    to: email,
+    subject: `📈 Market News Summary Today - ${date}`,
+    text: `Today's market news summary from Signalist`,
+    html: htmlTemplate,
   };
 
   await transporter.sendMail(mailOptions);
